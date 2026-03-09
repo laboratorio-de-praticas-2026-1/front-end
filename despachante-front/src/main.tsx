77c4eb2ba70beb9e55325488c643186bb9dd3fa7
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2026-03-09 00:38:17 -0300

    Chore: atualiza as classes de estilizacao tailwind

--- despachante-front/src/main.tsx
+++ despachante-front/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Importando a Montserrat nos pesos principais
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css'; // Normal
+import '@fontsource/montserrat/500.css'; // Medium
+import '@fontsource/montserrat/600.css'; // Semi-bold
+import '@fontsource/montserrat/700.css'; // Bold
 import './index.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
