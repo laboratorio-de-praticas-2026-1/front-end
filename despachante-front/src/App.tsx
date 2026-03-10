0fd1b575ad5117518fe2bac6daaf204c8a8a2442
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2026-03-10 03:21:46 -0300

    feature: cria navbar

--- despachante-front/src/App.tsx
+++ despachante-front/src/App.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { Navbar } from "@/components/layout/Navbar"
 import { FaReact } from "react-icons/fa"
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-100">
-      <Button className="flex items-center gap-2 text-lg">
-        <FaReact />
-        Ambiente configurado
-      </Button>
+      <Navbar />
     </div>
   )
 }
