6984707124062c5b9e96bb948e529760194a8444
anacardozo <anaflavia.cr.18@gmail.com>
2026-03-11 04:23:26 -0300

    feat: criação do componente form da header

--- despachante-front/src/App.tsx
+++ despachante-front/src/App.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui/button"
 import { FaReact } from "react-icons/fa"
+import FormsHeader from "./components/HeaderForm"
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-zinc-100">
-      <Button className="flex items-center gap-2 text-lg">
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <FormsHeader>
+
+      </FormsHeader>
+      {/* <Button className="flex items-center gap-2 text-lg">
         <FaReact />
         Ambiente configurado
-      </Button>
+      </Button> */}
+
     </div>
   )
 }
