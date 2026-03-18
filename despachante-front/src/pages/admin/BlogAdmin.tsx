82cbe2bcb6c77b82361c59770255e8f514b90c9c
João Paulo Goreri Rustichelli <joaopaulo.rustichelli@gmail.com>
2026-03-17 22:35:30 -0300

    feature: adiciona cabeçalho do cms blog

--- despachante-front/src/pages/admin/BlogAdmin.tsx
+++ despachante-front/src/pages/admin/BlogAdmin.tsx
@@ -1,10 +1,9 @@
+import BuscaBlog from "@/components/sections/BuscaCadastroBlog"
+
 export function BlogAdmin() {
   return (
     <div>
-      <h2 className="text-2xl font-bold text-zinc-800 mb-6">Gerenciar Blog</h2>
-      <div className="bg-white rounded-xl shadow-sm border border-zinc-200 p-6 min-h-[400px]">
-        <p className="text-zinc-500">A tabela de posts entrará aqui...</p>
+      <BuscaBlog></BuscaBlog>
       </div>
-    </div>
   );
 }
